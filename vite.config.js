fc7ff7e72fce72d956cbb64ddd91fd2cee9b2e34
raohasham <hashamrao2005@gmail.com>
2024-08-13 14:39:18 +0500

    initial upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-   base: "/anecdotes/"
+  base: "/anecdotes/",
+  plugins: [react()]
 })
